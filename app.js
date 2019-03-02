46d3e7dbb6f0b261b7f6c8e72ea184cc3a271c4a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-02 13:08:27 -0600

    Fix issue where ISBN info was missing from library table.

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ UI.prototype.addBookToList = function(book){
 	row.innerHTML = `
 	<td>${book.title}</td>
 	<td>${book.author}</td>
+	<td>${book.isbn}</td>
 	<td><a href="#" class="delete">X</a></td>
 	`
 	list.appendChild(row);
